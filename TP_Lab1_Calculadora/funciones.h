ea119efe447f9e2d4353dd7cbe208f3f145fb445
lucasdliker <lucasdliker@gmail.com>
2020-04-18 15:14:29 -0300

    No puedo usar eclipse, tuve que realizarlo en Codeblocks

---
+++ TP_Lab1_Calculadora/funciones.h
@@ -0,0 +1,73 @@
+
+
+/** \brief Funcion para ingresar la opcion del menu
+ * \param  Pregunta por la opcion a ingresar
+ * \return devuelve el numero de la opcion ingresada
+ *
+ */
+
+int askOption (void);
+
+/** \brief Funcion para ingresar el numero de los operandos
+ *
+ * \param pregunta por el numero a ingresar
+ * \return devuelve el numero ingresado al operador correspondiente
+ *
+ */
+
+
+void askNumber (int* numero);
+
+/** \brief Sumar: Funcion que suma los dos numeros ingresados.-
+ *
+ * \param int: Pide el primer numero ingresado.-
+ * \param int: pide el segundo numero ingresado.-
+ * \return devuelve el resultado de la operacion.-
+ *
+ */
+
+int sumar(int operadorA, int operadorB);
+
+/** \brief restar: Funcion que resta los dos numeros ingresados.-
+ *
+ * \param int: Pide el primer numero ingresado.-
+ * \param int: pide el segundo numero ingresado.-
+ * \return devuelve el resultado de la operacion.-
+ *
+ */
+int restar(int operadorA, int operadorB);
+
+/** \brief multiplicar: Funcion que multiplica los dos numeros ingresados.-
+ *
+ * \param int: Pide el primer numero ingresado.-
+ * \param int: pide el segundo numero ingresado.-
+ * \return devuelve el resultado de la operacion.-
+ *
+ */
+int multiplicar(int operadorA, int operadorB);
+
+/** \brief division: Funcion que divide los dos numeros ingresados.-
+ *
+ * \param int: Pide el primer numero ingresado.-
+ * \param int: pide el segundo numero ingresado.-
+ * \return devuelve el resultado de la operacion.-
+ *
+ */
+float dividir(int operadorA, int operadorB);
+
+/** \brief factorial de primer numero: Funcion que calcula el factorial.-
+ *
+ * \param int: Pide el primer numero ingresado.-
+ * \return devuelve el resultado de la operacion.-
+ *
+ */
+long long int factorialMain(int operador);
+
+/** \brief factorial del segundo numero: Funcion que calcula el factorial.-
+ *
+ * \param int: pide el segundo numero ingresado.-
+ * \return devuelve el resultado de la operacion.-
+ *
+ */
+
+//long long int factorialMainB(int operadorB);
